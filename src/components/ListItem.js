17f746ec431ae20fe3d13b205d5e91b5030600dd
David Bowen <dsbmusic328@gmail.com>
2018-03-07 11:47:48 -0500

    light styling of page

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-// import '../stylesheets/ListItem.css';
+import '../stylesheets/ListItem.css';
+import 'tachyons';
 
 class ListItem extends Component {
     render() {
         const { key2, value } = this.props;
         return (
-            <li className="listItem">
-                <p className="leftItem">{key2}</p>
-                <p className="rightItem">{value}</p>
+            <li className="listItem listFlexBox">
+                <span className="leftItem">{key2}:</span>
+                <span className="rightItem ">{value}</span>
             </li>
         );
     }
